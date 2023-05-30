760944fe96f5c2fc51cee8a228fb341c7551d5e0
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-05-29 20:10:26 -0400

    passed testing

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,16 +1,43 @@
 class Game:
     def __init__(self, title):
-        self.title = title
+        self._title = title
         self._results = []
         self._players = []
         
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and value:
+            self._title = value
+        else:
+            raise Exception
+            
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if new_result and isinstance(new_result, Result):
+            self._results.append(new_result)
+        return self._results
     
     def players(self, new_player=None):
         from classes.player import Player
-        pass
+        if new_player and isinstance(new_player, Player):
+            self._players.append(new_player)
+        return self._players
     
-    def average_score(self, player):
-        pass
+    def average_score(self, player): 
+        player_scores = []
+        for each_result in self._results:
+            if each_result.player == player:
+                player_scores.append(each_result.score)
+        total_scores = sum(player_scores)
+        average_score = total_scores / len(player_scores)
+        return average_score
+
+    # def average_score(self, player):
+        # player_scores = [r.score for r in self._results if r.player == player]
+        # if player_scores:
+        # return sum(player_scores) / len(player_scores)
+        # return 0
--- lib/debug.py
+++ lib/debug.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
     ipdb.set_trace()
+
